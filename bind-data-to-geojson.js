28ca0bc265ba797ee0ab8d6cac1b1d4a2e5d7746
Gabriel Florit <gabrielflorit@gmail.com>
2013-08-31 11:55:49 -0400

    don't include the entire datum. don't overwrite an existing feature property.

--- bind-data-to-geojson.js
+++ bind-data-to-geojson.js
@@ -1,16 +1,53 @@
-// TODO: warn if the new data will overwrite an existing property
-// TODO: don't include the entire datum
+var _ = require('lodash');
 
 module.exports.bindData = function(data, geojson, dataColumn, geojsonColumn, ignoreCase) {
+
+	// get geojson columns (don't include geojsonColumn)
+	var geojsonColumns = _(geojson.features)
+		.map(function(feature) {
+			return _.keys(feature.properties);
+		})
+		.flatten()
+		.uniq()
+		.sortBy(function(v, i) {
+			return v;	
+		})
+		.reject(function(v) {
+			return v === geojsonColumn;
+		})
+		.value();
+
+	// get data columns (don't include dataColumn)
+	var dataColumns = _(data)
+		.map(function(datum) {
+			return _.keys(datum);
+		})
+		.flatten()
+		.uniq()
+		.sortBy(function(v, i) {
+			return v;	
+		})
+		.reject(function(v) {
+			return v === dataColumn;
+		})
+		.value();
+
+	// get the intersection of geojson and data columns
+	var commonColumns = _.intersection(geojsonColumns, dataColumns);
+
+	// don't allow data overwrite
+	if (commonColumns.length) {
+		throw "Found duplicate columns: " + JSON.stringify(commonColumns, null, 4);
+	}
 
 	var noMatches = [];
 
 	// for each datum,
-	data.forEach(function(datum) {
+	_.each(data, function(datum) {
 
 		// find the geojson feature that contains a geojsonColumn property
 		// that matches dataColumn
-		var features = geojson.features.filter(function(feature) {
+		var features = _.filter(geojson.features, function(feature) {
 
 			var match = false;
 
@@ -28,10 +65,13 @@ module.exports.bindData = function(data, geojson, dataColumn, geojsonColumn, ign
 			return match;
 		});
 
-		features.forEach(function(feature) {
-			feature.properties.data = datum;
+		// add datum to each feature,
+		// without adding dataColumn
+		_.each(features, function(feature) {
+			_.assign(feature.properties, _.omit(datum, dataColumn));
 		});
 
+		// this datum doesn't have a matching feature
 		if (!features.length) {
 			noMatches.push(datum[dataColumn]);
 		}
